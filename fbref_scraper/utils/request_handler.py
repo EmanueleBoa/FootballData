0597270088397512c1205b80f4d37e75e71881c8
Emanuele Boattini <emanuele.boattini@gmail.com>
2024-08-14 19:33:47 +0200

    added request handler

---
+++ fbref_scraper/utils/request_handler.py
@@ -0,0 +1,14 @@
+import requests as requests
+
+from ..exceptions import RequestError
+
+
+class RequestHandler:
+    @staticmethod
+    def get(url: str) -> str:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            return response.text
+        except requests.RequestException as e:
+            raise RequestError(f"Request failed: {str(e)}")
