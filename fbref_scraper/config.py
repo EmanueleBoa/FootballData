85fd0a9aff3253e8fd16b19fb894efe5ff5de0e8
Emanuele Boattini <emanuele.boattini@gmail.com>
2024-08-15 11:21:41 +0200

    add config file

---
+++ fbref_scraper/config.py
@@ -0,0 +1,9 @@
+BASE_URL = 'https://fbref.com/en/'
+
+competition_name_to_id = {
+    'Premier League': '9',
+    'Serie A': '11',
+    'La Liga': '12',
+    'Ligue 1': '13',
+    'Bundesliga': '20'
+}
