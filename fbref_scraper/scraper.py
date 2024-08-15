d9b3fc100fd24e629ebc4910ecdf0e5954a377e7
Emanuele Boattini <emanuele.boattini@gmail.com>
2024-08-15 11:22:16 +0200

    change download params

--- fbref_scraper/scraper.py
+++ fbref_scraper/scraper.py
@@ -1,3 +1,4 @@
+from .config import BASE_URL, competition_name_to_id
 from .parsers import FixturesParser, MatchSummaryParser
 from .utils import RequestHandler
 
@@ -6,10 +7,22 @@ class FbRefScraper:
     def __init__(self):
         self.request_handler = RequestHandler()
 
-    def download_fixtures(self, url: str):
+    def download_fixtures(self, competition_name: str, season: str):
+        competition_id = competition_name_to_id.get(competition_name)
+        if competition_id is None:
+            raise Exception(f'Competition {competition_name} not supported! '
+                            f'Available competitions are: {list(competition_name_to_id.keys())}')
+        url = f'{BASE_URL}/comps/{competition_id}/{season}/schedule/'
         html = self.request_handler.get(url)
-        return FixturesParser().parse(html)
+        fixtures = FixturesParser().parse(html)
+        return {
+            'competition_id': competition_id,
+            'competition_name': competition_name,
+            'season': season,
+            'fixtures': fixtures
+        }
 
-    def download_match_events(self, url: str):
+    def download_match_events(self, match_id: str):
+        url = f'{BASE_URL}/matches/{match_id}/'
         html = self.request_handler.get(url)
         return MatchSummaryParser().parse(html)
