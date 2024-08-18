09163909f6b5e00f815335fd59c2f9c44f373252
Emanuele Boattini <emanuele.boattini@gmail.com>
2024-08-18 18:19:46 +0200

    add shots to scraper

--- fbref_scraper/parsers/__init__.py
+++ fbref_scraper/parsers/__init__.py
@@ -1,2 +1,3 @@
 from fbref_scraper.parsers.fixtures import FixturesParser
 from fbref_scraper.parsers.summary import MatchSummaryParser
+from fbref_scraper.parsers.shots import ShotsParser
--- fbref_scraper/scraper.py
+++ fbref_scraper/scraper.py
@@ -1,5 +1,5 @@
 from .config import BASE_URL, competition_name_to_id
-from .parsers import FixturesParser, MatchSummaryParser
+from .parsers import FixturesParser, MatchSummaryParser, ShotsParser
 from .utils import RequestHandler
 
 
@@ -7,7 +7,7 @@ class FbRefScraper:
     def __init__(self):
         self.request_handler = RequestHandler()
 
-    def download_fixtures(self, competition_name: str, season: str):
+    def download_fixtures(self, competition_name: str, season: str) -> dict:
         competition_id = self._get_competition_id(competition_name)
         url = f'{BASE_URL}/comps/{competition_id}/{season}/schedule/'
         html = self.request_handler.get(url)
@@ -19,16 +19,40 @@ class FbRefScraper:
             'fixtures': fixtures
         }
 
-    def download_match_events(self, match_id: str):
+    def download_match_report(self, match_id: str) -> dict:
         url = f'{BASE_URL}/matches/{match_id}/'
         html = self.request_handler.get(url)
-        return MatchSummaryParser().parse(html)
+        summary = MatchSummaryParser().parse(html)
+        shots = ShotsParser().parse(html)
+        return {
+            'match_id': match_id,
+            'summary': summary,
+            'shots': shots
+        }
+
+    def download_match_summary(self, match_id: str) -> dict:
+        url = f'{BASE_URL}/matches/{match_id}/'
+        html = self.request_handler.get(url)
+        summary = MatchSummaryParser().parse(html)
+        return {
+            'match_id': match_id,
+            'summary': summary
+        }
+
+    def download_match_shots(self, match_id: str) -> dict:
+        url = f'{BASE_URL}/matches/{match_id}/'
+        html = self.request_handler.get(url)
+        shots = ShotsParser().parse(html)
+        return {
+            'match_id': match_id,
+            'shots': shots
+        }
 
     @staticmethod
     def _get_competition_id(competition_name):
         competition_id = competition_name_to_id.get(competition_name)
         if competition_id is None:
             raise Exception(f'Competition {competition_name} not supported! '
-                            f'Available competitions are: {list(competition_name_to_id.keys())}')
+                            f'Supported competitions are: {list(competition_name_to_id.keys())}')
         return competition_id
 
